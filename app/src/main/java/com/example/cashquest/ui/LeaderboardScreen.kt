279f0a8c9427520b8622a406ad92a156a6732973
Eugen Sedlar <sedlar.eugen.biz@gmail.com>
2025-01-20 17:30:18 +0100

    FINISH LeaderboardScreen

--- app/src/main/java/com/example/cashquest/ui/LeaderboardScreen.kt
+++ app/src/main/java/com/example/cashquest/ui/LeaderboardScreen.kt
@@ -1,29 +1,100 @@
 package com.example.cashquest.ui
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.cashquest.ui.theme.CashQuestTheme
+
+
+data class LeaderboardEntry(val username: String, val amount: String)
 
 @Composable
 fun LeaderboardScreen() {
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.primaryContainer),
-        contentAlignment = Alignment.Center
+            .background(MaterialTheme.colorScheme.primaryContainer)
+            .padding(16.dp), // Added padding for spacing
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "Leaderboard",
+            text = "LEADERBOARD",
+            fontSize = 45.sp,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(sortedLeaderboard) { entry ->
+                LeaderboardItem(entry)
+                Spacer(modifier = Modifier.height(25.dp))
+            }
+        }
+    }
+}
+
+
+@Composable
+fun LeaderboardItem(entry: LeaderboardEntry) {
+    Column(
+        modifier = Modifier
+            .width(300.dp)
+            .background(MaterialTheme.colorScheme.onSurface)
+            .padding(vertical = 8.dp, horizontal = 16.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = entry.amount,
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colorScheme.primary
+            color = Color(0xFF276B45)
         )
+        Text(text = entry.username,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Medium,
+            color = MaterialTheme.colorScheme.onPrimary)
+    }
+}
+
+@Composable
+@Preview(
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(
+    uiMode = UI_MODE_NIGHT_NO,
+    name = "DefaultPreviewLight"
+)
+fun PreviewLeaderboardScreen(){
+    CashQuestTheme {
+        LeaderboardScreen()
     }
 }
